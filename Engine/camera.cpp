e17f4503c0df06d8bf04377e5eacd4fd256e736c
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2017-07-10 23:15:14 -0400

    Updated sln filters and removed comments from some cpp files

--- Engine/camera.cpp
+++ Engine/camera.cpp
@@ -1,6 +1,5 @@
 #include "camera.h"
 
-// Initialize CameraManager
 void InitializeCamera() {
 	camera.view_port = glm::vec4(0);
 	camera.position = glm::vec3(0);
@@ -10,35 +9,29 @@ void InitializeCamera() {
 	camera.is_view_dirty = false;
 }
 
-// Sets the camera view port
 void SetViewport(int x, int y, int width, int height) {
 	camera.view_port = glm::vec4(x, y, width, height);
 	glViewport(x, y, width, height);
 }
 
-// Set the camera position
 void SetPosition(const glm::vec3& position) {
 	camera.position = position;
 	camera.is_view_dirty = true;
 }
 
-// Sets the internal projection matrix
 void SetProjectionMatrix(float field_of_view, float aspect_ratio, float z_near_value, float z_far_value) {
 	camera.projection_matrix = glm::perspective(glm::radians(field_of_view), aspect_ratio, z_near_value, z_far_value);
 }
 
-// Sets the camera rotation
 void SetRotation(const glm::quat& rotation) {
 	camera.rotation = rotation;
 	camera.is_view_dirty = true;
 }
 
-// Set the Euler Angles
 void SetEulerAngles(const glm::vec3& euler_angles) {
 	camera.rotation = glm::quat(glm::radians(euler_angles));
 }
 
-// Translate the camera by delta
 void Translate(const glm::vec3& delta, bool local) {
 	if (local)
 		camera.position += camera.rotation * delta;
@@ -47,18 +40,15 @@ void Translate(const glm::vec3& delta, bool local) {
 	camera.is_view_dirty = true;
 }
 
-// Rotates the camera by provided quaternion
 void Rotate(const glm::quat& rotate_by) {
 	camera.rotation *= rotate_by;
 	camera.is_view_dirty = true;
 }
 
-// Returns the Euler Angles
 glm::vec3 GetEulerAngles() {
 	return glm::degrees(glm::eulerAngles(camera.rotation));
 }
 
-// Updates the view matrix
 glm::mat4 GetViewMatrix() {
 	if (camera.is_view_dirty) {
 		glm::mat4 translate = glm::translate(-camera.position);
@@ -69,7 +59,6 @@ glm::mat4 GetViewMatrix() {
 	return camera.view_matrix;
 }
 
-// Returns the projection matrix
 glm::mat4 GetProjectionMatrix() {
 	return camera.projection_matrix;
 }
